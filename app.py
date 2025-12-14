c9ad9952962a8749bead3f9a2de18c5b0aad3053
HANAZIRA20 <hanazira160@gmail.com>
2025-12-15 01:20:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.metrics import precision_recall_curve, average_precision_score
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 
@@ -300,5 +301,6 @@ st.markdown(
     "<p style='text-align:center;font-size:12px;'>Data Mining Project | Streamlit</p>",
     unsafe_allow_html=True
 )
+
 
 
